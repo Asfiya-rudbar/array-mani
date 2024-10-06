ceb4a01960661292a27c536eb1ec3a72227fb487
Asfiya Rudbar <asfiyarudbar@gmail.com>
2024-10-06 12:04:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// let x= 5;
+// function sayHello(){
+    
+//         let x = 2;
+   
+//         console.log(x);
+      
+        
+// }
+
+// console.log(x);
+// // sayHello();
+
+// function sayHello(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+// sayHello(2 , 4);
+
+// let dayOfWk = prompt("Day of week?");
+// let lower = dayOfWk.toLowerCase();
+
+
+// switch(dayOfWk) {
+//      case "Sat" :
+//      alert("Whoopee");
+//      break;
+//      case "Sun" :
+//      alert("Whoopee");
+//      break;
+//      case "Fri" :
+//      alert("TGIF!");
+//      break;
+//      default :
+//      alert("Shoot me now!");
+//      }
+    
+
